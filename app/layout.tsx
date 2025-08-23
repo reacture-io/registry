0ff309935812ff85c964b285af795a8ee7b3432f
Indy Van Canegem <i.vancanegem@faktion.com>
2025-08-23 23:31:08 +0200

    fix: layout scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 
-import { AppSidebar } from '@/components/blocks/sidebar/app-sidebar';
-import { Separator } from '@/components/ui/separator';
+import { AppSidebar } from "@/components/blocks/sidebar/app-sidebar";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from '@/components/ui/sidebar';
+} from "@/components/ui/sidebar";
 
-import './globals.css';
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Registry',
-  description: 'Custom registry for components',
+  title: "Registry",
+  description: "Custom registry for components",
 };
 
 const RootLayout = ({
@@ -31,22 +31,24 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => (
-  <html lang='en'>
-    <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  <html lang="en">
+    <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
+    >
       <SidebarProvider>
         <AppSidebar />
-        <SidebarInset>
-          <header className='group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear'>
-            <div className='flex items-center gap-2 px-4'>
-              <SidebarTrigger className='-ml-1' />
+        <SidebarInset className="h-screen overflow-hidden">
+          <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
               <Separator
-                orientation='vertical'
-                className='mr-2 data-[orientation=vertical]:h-4'
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
               />
             </div>
           </header>
-          <main className='m-4 flex h-full flex-col gap-4 overflow-auto'>
-            {children}
+          <main className="flex-1 m-4 overflow-y-auto">
+            <div className="flex flex-col gap-4">{children}</div>
           </main>
         </SidebarInset>
       </SidebarProvider>
